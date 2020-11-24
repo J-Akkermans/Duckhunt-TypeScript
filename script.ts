5ab591be884939f77c73327872380bca7f350470
J Akkermans <99044668@mydavinci.nl>
2020-11-24 10:42:47 +0100

    Able to move duck by function("DIRECTION")

--- script.ts
+++ script.ts
@@ -10,8 +10,32 @@ const step: number = 75;
 
 function fly(direction:string){ 
     console.log(direction)
+
     var x: string = window.getComputedStyle(img).left.replace('px', '');
     var y: string = window.getComputedStyle(img).top.replace('px','');
     var valueX:number = parseInt(x);
     var valueY:number = parseInt(y);
+
+    var moveXUP:number = valueY - step;
+    var moveXDOWN:number = valueY + step;
+
+    var moveYLEFT:number = valueX - step;
+    var moveYRIGHT:number = valueX + step;
+
+    switch (direction) {
+        case "n":
+            img.setAttribute("style", "top:"+ moveXUP +"px");
+            break;
+        case "e":
+            img.setAttribute("style", "left:"+ moveYRIGHT +"px");
+            break;
+        case "s":
+            img.setAttribute("style", "top:"+ moveXDOWN +"px");
+            break;
+        case "w":
+            img.setAttribute("style", "left:"+ moveYLEFT +"px");
+            break;
+    }
+
+    
 }
